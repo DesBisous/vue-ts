dbcaed89ec38670eaadd530198bc0715597269f9
iromfeed <>
2018-07-09 15:20:11 +0800

    初始化项目结构

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-
+    <el-row>
+      <el-button plain>朴素按钮</el-button>
+      <el-button type="primary" plain>主要按钮</el-button>
+      <el-button type="success" plain>成功按钮</el-button>
+      <el-button type="info" plain>信息按钮</el-button>
+      <el-button type="warning" plain>警告按钮</el-button>
+      <el-button type="danger" plain>危险按钮</el-button>
+    </el-row>
   </div>
 </template>
 
---
+++ src/components/common/SideBar.vue
@@ -0,0 +1,25 @@
+<script lang="tsx">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class SideBar extends Vue {
+
+  public render() {
+    return (
+      <el-aside width>
+        123
+      </el-aside>
+    );
+  }
+  public mounted() {
+    this.$message({
+      message: '恭喜你，这是一条成功消息',
+      type: 'success',
+    });
+  }
+}
+</script>
+
+<style scoped lang="less">
+
+</style>
